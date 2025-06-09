bb8650b56d24598f4645a3dc39ad8e7a1e8002ef
Ramon <ramonscampos@gmail.com>
2025-06-09 18:26:44 -0300

    loading and fetch data

---
+++ script.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const loadingOverlay = document.getElementById("loadingOverlay");
+
+	function showLoading() {
+		loadingOverlay?.classList.add("visible");
+	}
+
+	function hideLoading() {
+		loadingOverlay?.classList.remove("visible");
+	}
+
+	async function fetchPokemonDetails(url) {
+		try {
+			const id = url.split("/").filter(Boolean).pop();
+
+			if (!id) {
+				console.error("Não foi possível obter o ID do Pokémon da URL: ", url);
+				return null;
+			}
+
+			const response = await fetch(url);
+			if (!response.ok) {
+				console.error("Erro ao buscar dados da API para o Pokémon ID: ", id);
+				return null;
+			}
+
+			const pokemon = await response.json();
+
+			return {
+				id,
+				...pokemon,
+			};
+		} catch {}
+	}
+
+	async function fetchPokemons() {
+		try {
+			showLoading();
+
+			const response = await fetch(
+				"https://pokeapi.co/api/v2/pokemon?limit=251",
+			);
+
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+
+			const data = await response.json();
+
+			console.log(await fetchPokemonDetails(data.results[0].url));
+		} catch {
+		} finally {
+			hideLoading();
+		}
+	}
+
+	fetchPokemons();
+});
